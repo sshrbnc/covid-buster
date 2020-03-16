1065d94caed3b24c0a13c280ef5a5abd855e5935
John Carlo Ricablanca <john.ricablanca@awsys-i.com>
2020-03-16 14:25:26 +0800

    circle drawing and labels

--- js/map-test.js
+++ js/map-test.js
@@ -16,21 +16,103 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 //     }
 // ).addTo(mymap);
 
-var circle = L.circle([14.559161, 121.014919], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 50_000 //in meters
-}).addTo(mymap);
+//array regional circular objects declaration
 
-var popup = L.popup();
+var regions = [];
+var regionalData = [
+    {
+        "coordinates" : [14.6019, 121.0355], // San Juan
+        "place" : "San Juan City", 
+        "cases" : 15
+    },
+    {
+        "coordinates" : [14.6760, 121.0437], // Quezon City
+        "place" : "Quezon City", 
+        "cases" : 14
+    },
+    {
+        "coordinates" : [14.5547, 121.0244], // Makati
+        "place" : "Makati", 
+        "cases" : 14
+    },
+    {
+        "coordinates" : [14.5794, 121.0359], // Mandaluyong
+        "place" : "Mandaluyong", 
+        "cases" : 5
+    },
+    {
+        "coordinates" : [15.0794, 120.6200], // Pampanga
+        "place" : "Pampanga", 
+        "cases" : 3
+    },
+    {
+        "coordinates" : [14.6417, 120.4818], // Bataan
+        "place" : "Bataan", 
+        "cases" : 1
+    },
+    {
+        "coordinates" : [14.1407, 121.4692], // Laguna
+        "place" : "Laguna", 
+        "cases" : 1
+    },
+    {
+        "coordinates" : [13.7565, 121.0583], // Batangas
+        "place" : "Batangas", 
+        "cases" : 2
+    },
+    {
+        "coordinates" : [14.4791, 120.8970], // Cavite
+        "place" : "Cavite", 
+        "cases" : 2
+    },
+    {
+        "coordinates" : [14.6037, 121.3084], // Rizal
+        "place" : "Rizal", 
+        "cases" : 4
+    },
+    {
+        "coordinates" : [9.6282, 122.9888], // Negros Oriental
+        "place" : "Negros Oriental", 
+        "cases" : 1
+    },
+    {
+        "coordinates" : [9.8500, 124.1435], // Bohol
+        "place" : "Bohol", 
+        "cases" : 1
+    },
+    {
+        "coordinates" : [8.5046, 124.6220], // Misamis Oriental
+        "place" : "Misamis Oriental", 
+        "cases" : 1
+    },
+];
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+for(let i = 0; i < regionalData.length; i++){
+    regions[i] = L.circleMarker(regionalData[i]["coordinates"], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: regionalData[i]["cases"]
+    }).addTo(mymap);
+
+    regions[i].bindPopup(regionalData[i]["place"] + ": " + regionalData[i]["cases"] + " case/s");
 }
 
-mymap.on('click', onMapClick);
+// var circle = L.circle([14.559161, 121.014919], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 50_000 //in meters
+// }).addTo(mymap);
+
+// var popup = L.popup();
+
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(mymap);
+// }
+
+// mymap.on('click', onMapClick);
 
