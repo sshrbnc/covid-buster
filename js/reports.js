0038e7a17b382dadfc2444ee4eb795e09c6d65a0
Arjon Carlo Villamin <arjon.villamin@awsys-i.com>
2020-03-20 11:52:38 +0800

    Pull and display data from sheets

---
+++ js/reports.js
@@ -0,0 +1,44 @@
+var reportsData = "";
+
+window.addEventListener("load", async () => {
+    await authenticate().then(loadClient);
+    await execute();
+    reportsData = data;
+    populateReportsTable();
+});
+
+function populateReportsTable(){
+    $('#reportsPlaceholder').html("");
+
+    let head = 
+        "<table id='reportsTable' class='table table-striped' style='width:100%; background-color: lightgrey; color: #1D1128;'>" +
+        "<thead>" +
+        "<tr>" +
+        "<th>ID</th>" +
+        "<th>Age</th>" +
+        "<th>Sex</th>" +
+        "<th>Address</th>" +
+        "<th>Admitted?</th>" +
+        "<th>Symptoms</th>"
+        "</thead>";
+
+    let body = "<tbody>";
+
+    reportsData.forEach(function (report){
+        body +=
+            "<tr>" +
+            "<td>" + report[0] + "</td>" +
+            "<td>" + report[1] + "</td>" +
+            "<td>" + report[2] + "</td>" +
+            "<td>" + report[3] + "</td>" +
+            "<td>" + report[4] + "</td>" +
+            "<td>" + report[5] + "</td>" +
+            "</tr>";
+    });
+
+    $('#reportsPlaceholder').html(head + body + "</tbody></table>");
+
+    $('#reportsTable').DataTable({
+        responsive: true
+    });
+}
--- js/sheets.js
+++ js/sheets.js
@@ -1,11 +1,4 @@
-<script src="https://apis.google.com/js/api.js"></script>
-<script>
-  /**
-   * Sample JavaScript code for sheets.spreadsheets.values.batchGet
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/guides/code_samples#javascript
-   */
-
+  var data = "";    
   function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly"})
@@ -21,23 +14,25 @@
   // Make sure the client is loaded and sign-in is complete before calling this method.
   function execute() {
     return gapi.client.sheets.spreadsheets.values.batchGet({
-      "spreadsheetId": "1wdxIwD0b58znX4UrH6JJh_0IhnZP0YWn23Uqs7lHB6Q",
+      "spreadsheetId": "1AP8VfPAcRLv5l0zSeS6FK8_Dwqo1yXkrWPEcjlU1_g0",
       "dateTimeRenderOption": "FORMATTED_STRING",
       "majorDimension": "ROWS",
       "ranges": [
-        "'Database (Main)'"
+        "'reports'",
+        "'not recovered'",
+        "'deceased'",
+        "'recovered'"
       ],
       "valueRenderOption": "UNFORMATTED_VALUE"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
+                // console.log("Response", response);
+                data = response.result.valueRanges[0].values;
+                console.log(data);
               },
               function(err) { console.error("Execute error", err); });
   }
   gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: "403869365731-6c5k2kp62nbpn8epe7bb83a9a16sv8m8.apps.googleusercontent.com"});
   });
-</script>
-<button onclick="authenticate().then(loadClient)">authorize and load</button>
-<button onclick="execute()">execute</button>
