8e87f00afd807019b5d813f350cfb7ea75b5b0ec
Arjon Carlo Villamin <arjon.villamin@awsys-i.com>
2020-03-24 16:14:31 +0800

    Creates array of objects from shet data

--- js/sheets.js
+++ js/sheets.js
@@ -1,11 +1,5 @@
-  var data_reports;
-  var data_notRecovered;
-  var data_notRecovered_forValidation = [];
-  var data_notRecovered_critical = [];
-  var data_notRecovered_serious = [];
-  var data_notRecovered_stable = [];
-  var data_deceased;
-  var data_recovered;
+  var reports = [];
+
   function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly"})
@@ -22,57 +16,40 @@
   function execute() {
     return gapi.client.sheets.spreadsheets.values.batchGet({
 
-      // 1wdxIwD0b58znX4UrH6JJh_0IhnZP0YWn23Uqs7lHB6Q == r/covid spreadsheet
       // 1AP8VfPAcRLv5l0zSeS6FK8_Dwqo1yXkrWPEcjlU1_g0 == replica spreadsheet
 
       "spreadsheetId": "1AP8VfPAcRLv5l0zSeS6FK8_Dwqo1yXkrWPEcjlU1_g0",
       "dateTimeRenderOption": "FORMATTED_STRING",
       "majorDimension": "ROWS",
       "ranges": [
-        "'reports'",
-        "'not recovered'!A1:P338",
-        "'deceased'",
-        "'recovered'"
+        "'reports'"
       ],
       "valueRenderOption": "UNFORMATTED_VALUE"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
-                data_reports = response.result.valueRanges[0].values;
-                data_notRecovered = response.result.valueRanges[1].values;
-                for(var i=0; i<data_notRecovered.length; i++){
-                  switch(data_notRecovered[i][8]){
-                    case "(For Validation)":
-                      data_notRecovered_forValidation.push(data_notRecovered[i]);
-                      break;
-                    case "Critical":
-                      data_notRecovered_critical.push(data_notRecovered[i]);
-                      break;
-                    case "Serious":
-                      data_notRecovered_serious.push(data_notRecovered[i]);
-                      break;
-                    case "Stable":
-                      data_notRecovered_stable.push(data_notRecovered[i]);
-                      break;
-                  }
-                }
-                data_deceased = response.result.valueRanges[2].values;
-                data_recovered = response.result.valueRanges[3].values;
+                var temp_reports = response.result.valueRanges[0].values;
 
-                // console.log("Not Recovered");
-                // console.log(data_notRecovered);
-                // console.log("Validation");
-                // console.log(data_notRecovered_forValidation);
-                // console.log("Critical");
-                // console.log(data_notRecovered_critical);
-                // console.log("Serious");
-                // console.log(data_notRecovered_serious);
-                // console.log("Stable");
-                // console.log(data_notRecovered_stable);
-                // console.log("Deceased");
-                // console.log(data_deceased);
-                // console.log("Recovered");
-                // console.log(data_recovered);
+                var id = 0, name = "", dev = "", date_start = "", date_end = "", shortness_of_breath = "", fever = false, dry_cough = false, fatigue = false, sore_throat = false, nasal_congestion = false, runny_nose = false, diarrhea = false, others = false;
+                for(var i=1; i<temp_reports.length; i++){
+                  var report_details = { id, name, dev, date_start, date_end, shortness_of_breath, fever, dry_cough, fatigue, sore_throat, nasal_congestion, runny_nose, diarrhea, others }
+                
+                  report_details.id = temp_reports[i][0];
+                  report_details.name = temp_reports[i][1];
+                  report_details.dev = temp_reports[i][2];
+                  report_details.date_start = temp_reports[i][3];
+                  report_details.date_end = temp_reports[i][4];
+                  report_details.shortness_of_breath = temp_reports[i][5];
+                  report_details.fever = temp_reports[i][6];
+                  report_details.dry_cough = temp_reports[i][7];
+                  report_details.fatigue = temp_reports[i][8];
+                  report_details.sore_throat = temp_reports[i][9];
+                  report_details.nasal_congestion = temp_reports[i][10];
+                  report_details.runny_nose = temp_reports[i][11];
+                  report_details.diarrhea = temp_reports[i][12];
+                  report_details.others = temp_reports[i][13];
+                  reports.push(report_details);
+                }
               },
               function(err) { console.error("Execute error", err); });
   }
