ee8f8a70905d4ba37d06861250c19d23e0852b9e
Arjon Carlo Villamin <arjon.villamin@awsys-i.com>
2020-03-25 14:23:57 +0800

    Changed leave form (need style), submit fx in dash

--- js/dataloader_new.js
+++ js/dataloader_new.js
@@ -1,1 +1,52 @@
-console.log(reports);
+function populateReportsTable(){
+    $('#reportsPlaceholder').html("");
+
+    let head = 
+        "<table id='reportsTable' class='table table-striped' style='width:100%; background-color: lightgrey; color: #1D1128;'>" +
+        "<thead>" +
+        "<tr>" +
+        "<th>ID</th>"+
+        "<th>Name</th>" +
+        "<th>Dev</th>" +
+        "<th>Date Start</th>" +
+        "<th>Date End</th>" +
+        "<th>Shortness of Breath</th>" +
+        "<th>Fever</th>" +
+        "<th>Dry Cough</th>" +
+        "<th>Fatigue</th>" +
+        "<th>Sore Throat</th>" +
+        "<th>Nasal Congestion</th>" +
+        "<th>Runny Nose</th>" +
+        "<th>Diarrhea</th>" +
+        "<th>Others</th>" +
+        "</tr>" +
+        "</thead>";
+
+    let body = "<tbody>";
+
+    reportsData.forEach(function (report){
+        body +=
+            "<tr>" +
+            "<td>" + report.id + "</td>" +
+            "<td>" + report.name + "</td>" +
+            "<td>" + report.dev + "</td>" +
+            "<td>" + report.date_start + "</td>" +
+            "<td>" + report.date_end + "</td>" +
+            "<td>" + report.shortness_of_breath + "</td>" +
+            "<td>" + report.fever + "</td>" +
+            "<td>" + report.dry_cough + "</td>" +
+            "<td>" + report.fatigue + "</td>" +
+            "<td>" + report.sore_throat + "</td>" +
+            "<td>" + report.nasal_congestion + "</td>" +
+            "<td>" + report.runny_nose + "</td>" +
+            "<td>" + report.diarrhea + "</td>" +
+            "<td>" + report.others + "</td>" +
+            "</tr>";
+    });
+
+    $('#reportsPlaceholder').html(head + body + "</tbody></table>");
+
+    $('#reportsTable').DataTable({
+        responsive: true
+    });
+}
--- js/reports.js
+++ js/reports.js
@@ -1,70 +1,3 @@
-var reportsData = "";
-
-window.addEventListener("load", async () => {
-    await authenticate().then(loadClient);
-    await init();
-});
-
-async function init(){
-    reportsData = "";
-    await execute();
-    reportsData = reports;
-    populateReportsTable();
-}
-
-function populateReportsTable(){
-    $('#reportsPlaceholder').html("");
-
-    let head = 
-        "<table id='reportsTable' class='table table-striped' style='width:100%; background-color: lightgrey; color: #1D1128;'>" +
-        "<thead>" +
-        "<tr>" +
-        "<th>ID</th>" +
-        "<th>Name</th>" +
-        "<th>Dev</th>" +
-        "<th>Date Start</th>" +
-        "<th>Date End</th>" +
-        "<th>Shortness of Breath</th>" +
-        "<th>Fever</th>" +
-        "<th>Dry Cough</th>" +
-        "<th>Fatigue</th>" +
-        "<th>Sore Throat</th>" +
-        "<th>Nasal Congestion</th>" +
-        "<th>Runny Nose</th>" +
-        "<th>Diarrhea</th>" +
-        "<th>Others</th>" +
-        "</tr>" +
-        "</thead>";
-
-    let body = "<tbody>";
-
-    reportsData.forEach(function (report){
-        body +=
-            "<tr>" +
-            "<td>" + report.id + "</td>" +
-            "<td>" + report.name + "</td>" +
-            "<td>" + report.dev + "</td>" +
-            "<td>" + report.date_start + "</td>" +
-            "<td>" + report.date_end + "</td>" +
-            "<td>" + report.shortness_of_breath + "</td>" +
-            "<td>" + report.fever + "</td>" +
-            "<td>" + report.dry_cough + "</td>" +
-            "<td>" + report.fatigue + "</td>" +
-            "<td>" + report.sore_throat + "</td>" +
-            "<td>" + report.nasal_congestion + "</td>" +
-            "<td>" + report.runny_nose + "</td>" +
-            "<td>" + report.diarrhea + "</td>" +
-            "<td>" + report.others + "</td>" +
-            "</tr>";
-    });
-
-    $('#reportsPlaceholder').html(head + body + "</tbody></table>");
-
-    $('#reportsTable').DataTable({
-        responsive: true
-    });
-}
-
 async function submitReport(){
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var date_start_val = new Date(document.getElementById("date_start").value);
@@ -72,8 +5,8 @@ async function submitReport(){
 
     var date_start = months[date_start_val.getMonth()] + " " + date_start_val.getDate();
     var date_end = months[date_end_val.getMonth()] + " " + date_end_val.getDate();
-    var name = "Matthew Murdock";
-    var dev = "Dev D";
+    var name = "Luna Lovegood";
+    var dev = "Dev H";
 
     if(date_start==""  || date_end == ""){
         alert("Please fill in all required fields.");
